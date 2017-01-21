ecca6f54f45f42da550b279b7b341458c00cf9ac
jewelk <jewelk18@gmail.com>
2017-01-21 15:37:41 +0600

    Add files via upload

---
+++ NRF_2CH TX RX/NRF_Receiver/NRF_Receiver.ino
@@ -0,0 +1,24 @@
+#include <Servo.h>
+#include <SPI.h>
+#include "RF24.h"
+Servo servo1;
+Servo servo2;
+RF24 radio(9,10);
+const uint64_t pipe = 0xE8E8F0F0E1LL; 
+int msg[1]; 
+int data; 
+int pos;
+void setup()
+{
+servo1.attach(2); 
+servo2.attach(6);
+radio.begin();
+radio.openReadingPipe(1,pipe); 
+radio.startListening();
+}
+void loop()
+{
+if (radio.available())radio.read(msg, 1);
+if (msg[0] <128 && msg[0] >-1)data = msg[0], pos = map(data, 0, 127, 7, 177),servo1.write(pos);
+if (msg[0] >127 && msg[0] <255)data = msg[0], pos = map(data, 128, 254, 9, 177),servo2.write(pos);
+}
---
+++ NRF_2CH TX RX/transmitter/transmitter.ino
@@ -0,0 +1,17 @@
+#include <SPI.h>
+#include "RF24.h" 
+RF24 radio(9,10);
+const uint64_t pipe = 0xE8E8F0F0E1LL; 
+int msg[1]; 
+int Joy_X = A0; 
+int val_1; 
+int Joy_Y = A1; 
+int val_2; 
+void setup(void){
+radio.begin();
+radio.openWritingPipe(pipe); 
+}
+void loop() {
+val_1 = analogRead(Joy_X),val_1 = map(val_1, 0, 1023, 55, 100),msg[0] = val_1,radio.write(msg, 1);
+val_2 = analogRead(Joy_Y),val_2 = map(val_2, 0, 1023, 128, 255),msg[0] = val_2,radio.write(msg, 1);
+}
